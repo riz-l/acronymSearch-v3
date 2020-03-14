ce1392157a61b423a44413a48925ac70ccf5246d
Riz <56162066+riz-l@users.noreply.github.com>
2020-03-14 00:51:01 +0000

    Uploaded files to-date
    Added PWA files (sw.js, manifest.json, etc.)
      - PWA functionality isn't 100%; WIP

---
+++ assets/js/db.js
@@ -0,0 +1,15 @@
+// Real-time listener
+db.collection("acronymsAndDefinitions").onSnapshot(snapshot => {
+  // Console.log displays any additions or removals from db
+  //   console.log(snapshot.docChanges());
+  snapshot.docChanges().forEach(change => {
+    // Console.log displays data, fields, and id
+    // console.log(change, change.doc.data(), change.doc.id);
+    if (change.type === "added") {
+      // Add the document data to the DOM
+    }
+    if (change.type === "removed") {
+      // Remove the document data from the DOM
+    }
+  });
+});
---
+++ assets/js/ui.js
@@ -0,0 +1,1 @@
+// UI.js File Placeholder
